bd2e9e8659083f3f3b5006ff65398668af573417
mades eladrian <mades.eladrian@gmail.com>
2022-07-23 18:25:16 -0300

    move Post BaseModel to schemas folder

--- app/main.py
+++ app/main.py
@@ -1,16 +1,10 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends
-from pydantic import BaseModel
-from . import models
+from . import models, schemas
 from .database import get_db, engine
 from sqlalchemy.orm import Session
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-class Post(BaseModel):
-  title: str
-  content: str
-  published: bool = True
 
 @app.get("/")
 def root():
@@ -22,7 +16,7 @@ def get_posts(db: Session = Depends(get_db)):
   return {"data": posts}
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post, db: Session = Depends(get_db)):
+def create_posts(post: schemas.Post, db: Session = Depends(get_db)):
   new_post = models.Post(**post.dict())
   db.add(new_post)
   db.commit()
@@ -47,7 +41,7 @@ def delete_post(id: int, db: Session = Depends(get_db)):
   return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post, db: Session = Depends(get_db)):
+def update_post(id: int, post: schemas.Post, db: Session = Depends(get_db)):
   post_query = db.query(models.Post).filter(models.Post.id == id)
   updated_post = post_query.first()
   if updated_post == None:
