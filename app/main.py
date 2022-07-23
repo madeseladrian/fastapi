84c65743ec4d3a98f5e8daee7cacaeeb81acb99d
mades eladrian <mades.eladrian@gmail.com>
2022-07-23 19:55:17 -0300

    create a response model

--- app/main.py
+++ app/main.py
@@ -13,26 +13,28 @@ def root():
 @app.get("/posts")
 def get_posts(db: Session = Depends(get_db)):
   posts = db.query(models.Post).all()
-  return {"data": posts}
+  return posts
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
   new_post = models.Post(**post.dict())
   db.add(new_post)
   db.commit()
   db.refresh(new_post)
-  return {"data": new_post}
+  return new_post
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model=schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db)):
   post = db.query(models.Post).filter(models.Post.id == id).first()
+  
   if not post:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
-  return {"post_detail": post}
+  return post
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
   post = db.query(models.Post).filter(models.Post.id == id)
+
   if post.first() == None:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
   
@@ -40,12 +42,14 @@ def delete_post(id: int, db: Session = Depends(get_db)):
   db.commit()
   return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-@app.put("/posts/{id}")
+@app.put("/posts/{id}", response_model=schemas.Post)
 def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
   post_query = db.query(models.Post).filter(models.Post.id == id)
   updated_post = post_query.first()
+
   if updated_post == None:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+  
   post_query.update(post.dict(), synchronize_session=False)
   db.commit()
-  return {"data": post_query.first()}
+  return post_query.first()
