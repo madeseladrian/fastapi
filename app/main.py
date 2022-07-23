f5464d607da1d6272381f85bf66d3ccb0dd32788
mades eladrian <mades.eladrian@gmail.com>
2022-07-23 18:20:16 -0300

    remove helpers methods

--- app/main.py
+++ app/main.py
@@ -12,21 +12,6 @@ class Post(BaseModel):
   content: str
   published: bool = True
 
-my_posts = [
-  {"title": "title of post 1", "content": "content of post 1", "id": 1},
-  {"title": "favorite foods", "content": "I like pizza", "id": 2},
-]
-
-def find_post(id):
-  for p in my_posts:
-    if p['id'] == id: 
-      return p
-
-def find_index_post(id):
-  for i, p in enumerate(my_posts):
-    if p['id'] == id:
-      return i
-
 @app.get("/")
 def root():
   return {"message": "Welcome to FastApi"}
