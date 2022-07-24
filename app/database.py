73c1106b7640e21560517f5e718dff215b0435dc
mades eladrian <mades.eladrian@gmail.com>
2022-07-24 16:26:55 -0300

    add environment variables

--- app/database.py
+++ app/database.py
@@ -1,9 +1,14 @@
-from pickle import FALSE
+from .config import settings
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:123456@localhost/fastapi'
+SQLALCHEMY_DATABASE_URL = f'postgresql://\
+{settings.database_username}:\
+{settings.database_password}@\
+{settings.database_hostname}:\
+{settings.database_port}/\
+{settings.database_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
--- app/oauth2.py
+++ app/oauth2.py
@@ -1,4 +1,5 @@
 from . import database, models, schemas
+from .config import settings
 from datetime import datetime, timedelta
 from fastapi import Depends, HTTPException, status 
 from fastapi.security import OAuth2PasswordBearer
@@ -7,9 +8,9 @@ from sqlalchemy.orm import Session
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
-SECRET_KEY = "08d25e094faa6ca2516c818166b7a9563b98f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
 def create_access_token(data: dict):
   to_encode = data.copy()
