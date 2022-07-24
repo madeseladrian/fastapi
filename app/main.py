532e6fd752d6caf02a1843ae3c1f20e64f4a4354
mades eladrian <mades.eladrian@gmail.com>
2022-07-24 18:36:12 -0300

    disable sqlalchemy to create engine

--- app/main.py
+++ app/main.py
@@ -1,9 +1,5 @@
 from fastapi import FastAPI
-from . import models
-from .database import engine
 from .routers import login, post, user, vote
-
-models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
