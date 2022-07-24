ee7ebb8c2fdf657cfb884ba7ec37539b61b238f7
mades eladrian <mades.eladrian@gmail.com>
2022-07-24 11:22:58 -0300

    update Post schemas

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from .database import Base
-from sqlalchemy import TIMESTAMP, Boolean, Column, Integer, String
+from sqlalchemy import TIMESTAMP, Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.sql.expression import text
 
 class Post(Base):
@@ -12,6 +12,7 @@ class Post(Base):
   created_at = Column(
     TIMESTAMP(timezone=True), nullable=False, server_default=text('now()')
   )
+  owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
 
 class User(Base):
   __tablename__ = "users"
