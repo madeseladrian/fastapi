0645c936c41880714f55c07a681aff118d1d5e38
mades eladrian <mades.eladrian@gmail.com>
2022-07-24 17:09:11 -0300

    add vote api

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from . import models
 from .database import engine
-from .routers import login, post, user
+from .routers import login, post, user, vote
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -10,6 +10,7 @@ app = FastAPI()
 app.include_router(login.router)
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(vote.router)
 
 @app.get("/")
 def root():
--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, conint, EmailStr 
 from datetime import datetime
 
 class UserCreate(BaseModel):
@@ -41,3 +41,7 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
   id: Optional[str] = None
+
+class Vote(BaseModel):
+  post_id: int
+  dir: conint(le=1)
