4c64ffc886f7ad1289e2e435abbc3889f9c90a1c
mades eladrian <mades.eladrian@gmail.com>
2022-07-23 23:12:27 -0300

    add login route with cryptography

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from . import models
 from .database import engine
-from .routers import post, user
+from .routers import login, post, user
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.include_router(login.router)
 app.include_router(post.router)
 app.include_router(user.router)
 
