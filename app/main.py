d42d987cb6cf56825c6f0d8240d3d7ee2531fee1
mades eladrian <mades.eladrian@gmail.com>
2022-07-23 21:37:08 -0300

    add create user api

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends
-from . import models, schemas
+from . import models, schemas, utils
 from .database import get_db, engine
 from sqlalchemy.orm import Session
-models.Base.metadata.create_all(bind=engine)
 from typing import List
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -54,3 +55,14 @@ def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)
   post_query.update(post.dict(), synchronize_session=False)
   db.commit()
   return post_query.first()
+
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+  hashed_password = utils.hash(user.password)
+  user.password = hashed_password
+
+  new_user = models.User(**user.dict())
+  db.add(new_user)
+  db.commit()
+  db.refresh(new_user)
+  return new_user
